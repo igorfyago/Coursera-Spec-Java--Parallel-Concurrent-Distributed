3a6732bb1c1688a4ccb69273c7adb379a5c69fd8
Igor F. Yago <igor.yago@gmail.com>
2019-02-17 21:00:40 +0000

    w1

--- Concurrent/miniproject_1/src/main/java/edu/coursera/concurrent/CoarseLists.java
+++ Concurrent/miniproject_1/src/main/java/edu/coursera/concurrent/CoarseLists.java
@@ -24,7 +24,7 @@ public final class CoarseLists {
          */
 
         private final ReentrantLock lock = new ReentrantLock();
-        
+
         /**
          * Default constructor.
          */
